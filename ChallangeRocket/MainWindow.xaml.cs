d8f6987bcbefa6d5418e674797bfe3425e0843dc
toumash <t.dluski@npc.pl>
2019-01-23 19:56:07 +0100

    newest, working version

--- ChallangeRocket/MainWindow.xaml.cs
+++ ChallangeRocket/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using CsQuery;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -23,6 +25,58 @@ namespace ChallangeRocket
         public MainWindow()
         {
             InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
         }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            await Update();
+            var timer = new Timer(10000);
+            timer.Elapsed += async (s, args) => await Update();
+            timer.Start();
+        }
+
+        private async Task Update()
+        {
+            var dom = CQ.CreateFromUrl("https://challengerocket.com/bankitup2018/works");
+            var elements = dom.Select("#all > div.challenge-group > section > div > div > ul > li");
+            var url = elements.Select("h4 > a");
+            var titles = url.Elements.Select(x => x.InnerText).ToList();
+            var votes = elements.Select(".icon-vote").Elements.Select(x => x.InnerText).ToList();
+
+            var projects = new List<Project>();
+            for (int i = 0; i < elements.Length; i++)
+            {
+                var votesTyped = int.Parse(votes[i].Split(':')[1]);
+                projects.Add(new Project()
+                {
+                    Votes = votesTyped,
+                    Name = titles[i]
+                });
+            }
+
+            var twoFirst = projects.OrderByDescending(x => x.Votes).Take(2).ToList().OrderByDescending(x => x.Name);
+
+            var sum = twoFirst.Sum(x => x.Votes);
+            var first = twoFirst.First().Votes;
+
+            await App.Current.Dispatcher.Invoke(async () =>
+            {
+                gauge.From = 0;
+                gauge.Value = first;
+                gauge.To = sum;
+                tb_firstTeam.Text = twoFirst.First().Name;
+                tb_firstTeam_Votes.Text = twoFirst.First().Votes.ToString();
+                tb_secondTeam.Text = twoFirst.Skip(1).First().Name;
+                tb_secondTeam_Votes.Text = twoFirst.Skip(1).First().Votes.ToString();
+                tb_difference.Text = (twoFirst.First().Votes - twoFirst.Skip(1).First().Votes).ToString();
+                tb_lastUpdate.Text = DateTime.Now.ToString();
+            });
+        }
+    }
+    public class Project
+    {
+        public int Votes { get; set; }
+        public string Name { get; set; }
     }
 }
